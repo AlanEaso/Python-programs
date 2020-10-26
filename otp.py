7d730c7081f58ae7f6e357fa450c8e51df9ab746
AlanEaso <41380128+AlanEaso@users.noreply.github.com>
2020-10-26 21:03:12 +0530

    Update otp.py

--- otp.py
+++ otp.py
@@ -5,13 +5,13 @@ from twilio.rest import Client
 
 otp = randint(1000,10000)
 print(otp)
-asid = 'AC1419730200772fd1dd0764c9eb977d5d'
-atoken= '059a9f16d2892e37ec39a212ef827ffd'
+asid = ''
+atoken= ''
 cl = Client(asid,atoken)
 message = cl.messages.create(
     body='Your otp is '+str(otp),
-    from_= '+12566662802' ,
-    to='+918547442300'
+    from_= '' ,
+    to=''
 )
 
 print(message.sid)
