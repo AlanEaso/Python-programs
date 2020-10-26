1d084ef917b7d8951d7016998d7785666fd4f83c
AlanEaso <41380128+AlanEaso@users.noreply.github.com>
2020-10-26 21:06:38 +0530

    Update otp.py

--- otp.py
+++ otp.py
@@ -1,16 +1,17 @@
 #A simole OTP Generator using random
+#For this to work one should have a twilio account. It's free for trial runs
 
 from random import *
 from twilio.rest import Client
 
 otp = randint(1000,10000)
 print(otp)
-asid = ''
-atoken= ''
+asid = ''  #account sid from twilio account
+atoken= '' #from twilio account
 cl = Client(asid,atoken)
 message = cl.messages.create(
     body='Your otp is '+str(otp),
-    from_= '' ,
+    from_= '' , #trial number you get from twilio
     to=''
 )
 
